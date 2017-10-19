36a306944ec5ec0b92adf9ea7434ca1f3c5d890e
Kristoffermc <kristoffermchristensen@gmail.com>
2017-10-19 03:43:07 +0200

    Address Controller video 110

---
+++ CustomerRestAPIi/Controllers/AddressController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CustomerAppBLL;
+using CustomerAppBLL.BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerRestAPIi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/addresses")]
+    public class AddressController : Controller
+    {
+
+        BLLFacade facade = new BLLFacade();
+
+        [HttpPost]
+        public IActionResult Post([FromBody]AddressBO address)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(facade.AddressService.Create(address));
+        }
+    }
+}
